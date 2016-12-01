444c23f455c07c404b8aaead5bcb46c5517bb514
Sylvia van Os <iamsylvie@openmailbox.org>
2016-12-01 21:23:18 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Launcher module for Pext
+This module allows [Pext](https://github.com/Pext/Pext) to launch applications
+in $PATH. Currently, this module does not parse .desktop files, it simply lists
+every executable file in $PATH.
+
+# License
+GPLv3+.
---
+++ __init__.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+# Pext launcher module is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from os import access, environ, listdir, pathsep, X_OK
+from os.path import isfile, join
+from subprocess import Popen
+
+from pext_base import ModuleBase
+from pext_helpers import Action
+
+
+class Module(ModuleBase):
+    def init(self, settings, q):
+        self.q = q
+
+        self._get_entries()
+
+    def _get_entries(self):
+        executables = []
+
+        for path in environ['PATH'].split(pathsep):
+            for executable in listdir(path):
+                fullname = join(path, executable)
+                if isfile(fullname) and access(fullname, X_OK):
+                    executables.append(executable)
+
+        self.q.put([Action.replace_entry_list, sorted(executables)])
+
+    def stop(self):
+        pass
+
+    def selection_made(self, selection):
+        if len(selection) == 1:
+            Popen(selection[0]["value"])
+            self.q.put([Action.close])
+
+    def process_response(self, response):
+        pass
